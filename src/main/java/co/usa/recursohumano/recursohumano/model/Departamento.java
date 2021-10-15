b4e6a56757b2f5129db2e8fe9a1266fb846ebf57
Joluguar75 <jorge.gutierrez.mt@correo.usa.edu.co>
2021-10-15 18:17:49 -0500

    Inicializacion de proyecto

---
+++ src/main/java/co/usa/recursohumano/recursohumano/model/Departamento.java
@@ -0,0 +1,55 @@
+package co.usa.recursohumano.recursohumano.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="departamento")
+public class Departamento implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer numId;
+    private String nombreDepartamento;
+    private Date fechaCreacionDate;
+
+    // Realizamos la relacion
+    @OneToMany(cascade={CascadeType.PERSIST}, mappedBy = "departamento")
+    @JsonIgnoreProperties("departamento")
+    
+    private List<Empleado> empleados;
+
+
+    public List<Empleado> getEmpleados() {
+        return empleados;
+    }
+    public void setEmpleados(List<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+    public Integer getNumId() {
+        return numId;
+    }
+    public Date getFechaCreacionDate() {
+        return fechaCreacionDate;
+    }
+    public void setFechaCreacionDate(Date fechaCreacionDate) {
+        this.fechaCreacionDate = fechaCreacionDate;
+    }
+    public void setNumId(Integer numId) {
+        this.numId = numId;
+    }
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+
+    
+    
+}
---
+++ src/main/java/co/usa/recursohumano/recursohumano/service/DepartamentoServicio.java
@@ -0,0 +1,44 @@
+package co.usa.recursohumano.recursohumano.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.usa.recursohumano.recursohumano.model.Departamento;
+import co.usa.recursohumano.recursohumano.repository.DepartamentoRepositorio;
+
+@Service
+public class DepartamentoServicio {
+    @Autowired
+    private DepartamentoRepositorio departamentoRepositorio;
+
+    public List<Departamento>getAll(){
+        return departamentoRepositorio.getAll();
+
+    }
+
+    public Optional<Departamento>getDepartamento(int id){
+        return departamentoRepositorio.getDepartamento(id);
+    }
+
+    public Departamento save(Departamento dpto){
+        //verificamos si el departamento es nuevo de ser asi guarda
+
+        if (dpto.getNumId()==null) {
+            return departamentoRepositorio.save(dpto);
+
+        }else{ //Si el objeto viene con un numId verificamos si existe o no
+            Optional<Departamento> consulta=departamentoRepositorio.getDepartamento(dpto.getNumId());
+            if(consulta.isEmpty()) {
+                return departamentoRepositorio.save(dpto);
+
+            }else{
+                return dpto;
+            }
+
+        }
+    }
+    
+}
